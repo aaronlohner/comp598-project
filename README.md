ed1aaef7f7112d9b372fbe3cbea2f29f4fba252b
alohne <aaron.lohner@mail.mcgill.ca>
2021-11-22 17:57:50 -0500

    retrieve tweets using tweepy apiv1.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # comp598-project
+
+Link to overleaf: https://www.overleaf.com/4226275318ntfzndvgjsvg
--- src/collect_tweets.py
+++ src/collect_tweets.py
@@ -5,36 +5,38 @@ import requests
 import json
 import tweepy as tw
 
+
 parent_dir = Path(__file__).parent.resolve()
 consumer_key = None
 consumer_secret = None
 access_token = None
 access_token_secret = None
+bearer_token = None
 auth = None
-api = None
+client = None
+
 
 def initialize():
-    global consumer_key, consumer_secret, access_token, access_token_secret, auth, api
+    global consumer_key, consumer_secret, access_token, access_token_secret, auth, bearer_token, client, api
     with open(osp.join(parent_dir, '..', 'keys.txt')) as f:
-        consumer_key, consumer_secret, access_token, access_token_secret, _ = f.read().splitlines()
+        consumer_key, consumer_secret, access_token, access_token_secret, bearer_token = f.read().splitlines()
 
     auth = tw.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tw.API(auth, wait_on_rate_limit=True)
+    #client = tw.Client(bearer_token, consumer_key, consumer_secret, access_token, access_token_secret, return_type=dict, wait_on_rate_limit=True)
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output')#, required=True)
     args = parser.parse_args()
 
-    headers = {'User-Agent':'windows:university.project.app:v1.0'}
-    payload = {}
-
-    fetch_and_dump(payload, headers, args.subreddit, args.output)
-
 if __name__ == '__main__':
     initialize()
-    # date_since = '2021-11-18'
-    # for tweet in api.search_tweets(q='#covid',
-    #           lang="en").items(5):
-    #     print(tweet.text)
+
+    # assert not "retweeted"
+    tweets = api.search_tweets('#covid', lang='en', result_type='popular', count=1001)
+    for tweet in tweets:
+        print(f'text: {tweet.text}, created: {tweet.created_at}')
+    #print(api.reverse_geocode(56.448727, -102.745408, max_results = 5))
